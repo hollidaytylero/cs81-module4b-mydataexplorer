3b2fd1e65533cb1de029f202c4abf7c933169724
hollidaytylero <holliday_tyler_oli01@student.smc.edu>
2026-07-07 05:50:29 +0000

    Weekly Journal - 7 days

---
+++ myDataJournal.js
@@ -0,0 +1,63 @@
+// Weekly personal data journal
+// Predictions:
+// Which day had the most screen time? Weekends, Saturday and Sunday
+// Best focus day? Monday
+// Is more caffeine helping? No, I don't drink a lot of caffeine because I am still 15.
+
+const weekData = [
+  {
+    day: "Monday",
+    sleepHours: 7.5,
+    screenTime: 2,
+    mood: "Focused",
+    caffeineIntake: 0,
+    focusLevel: 8
+  },
+{
+    day: "Tuesday",
+    sleepHours: 8.5,
+    screenTime: 3,
+    mood: "Energized",
+    caffeineIntake: 0,
+    focusLevel: 7
+  },
+  {
+    day: "Wednesday",
+    sleepHours: 8,
+    screenTime: 1,
+    mood: "Productive",
+    caffeineIntake: 1,
+    focusLevel: 9
+  },{
+    day: "Thursday",
+    sleepHours: 8,
+    screenTime: 1.5,
+    mood: "Good",
+    caffeineIntake: 1,
+    focusLevel: 7
+  },
+  {
+    day: "Friday",
+    sleepHours: 9,
+    screenTime: 3,
+    mood: "Excited",
+    caffeineIntake: 1,
+    focusLevel: 6
+  },
+  {
+    day: "Saturday",
+    sleepHours: 9,
+    screenTime: 4,
+    mood: "Relaxed",
+    caffeineIntake: 0,
+    focusLevel: 5
+  },
+  {
+    day: "Sunday",
+    sleepHours: 8.5,
+    screenTime: 1.5,
+    mood: "Calm",
+    caffeineIntake: 0,
+    focusLevel: 5
+  }
+];
